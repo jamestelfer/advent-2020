f63ccfce0a023e5a6c48e885cf1e285541db2263
James Telfer <792299+jamestelfer@users.noreply.github.com>
2020-12-08 13:04:08 +0000

    Day 3 part 2

--- advent/Day3.cs
+++ advent/Day3.cs
@@ -10,21 +10,45 @@ class Day3
     {
         char[][] map = await Read();
 
+        var counts = new[]{
+            new { x = 1, y = 1 },
+            new { x = 1, y = 3 },
+            new { x = 1, y = 5 },
+            new { x = 1, y = 7 },
+            new { x = 2, y = 1 }
+        };
+
+        var calculated = counts.Select(c => TreeCount(map, c.y, c.x));
+
+        foreach (var res in calculated)
+        {
+            Console.WriteLine($"Trees: {res}");
+        }
+
+        long total = calculated.Aggregate(1L, (total, next) => total * next);
+
+        Console.WriteLine(total);
+    }
+
+    private static int TreeCount(char[][] map, int yStep, int xStep)
+    {
         int y = 0;
         int max = map[0].Length;
         int treeCount = 0;
 
-        foreach (char[] step in map)
+        for (int x = 0; x < map.Length; x += xStep)
         {
+            char[] step = map[x];
+
             if (step[y] == '#')
             {
                 treeCount++;
             }
 
-            y = (y + 3) % max;
+            y = (y + yStep) % max;
         }
 
-        Console.WriteLine($"Trees: {treeCount}");
+        return treeCount;
     }
 
     private async Task<char[][]> Read()
